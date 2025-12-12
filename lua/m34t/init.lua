40c202a075158d8e7477ce586c51188f87195100
slavar <m3atyy@gmail.com>
2025-12-12 18:47:17 +0500

    Add relative line number

--- lua/m34t/init.lua
+++ lua/m34t/init.lua
@@ -5,6 +5,9 @@ vim.cmd([[set spell]])
 vim.cmd([[set spelllang=en,ru]])
 vim.cmd([[set spelloptions=camel]])
 
+vim.opt.relativenumber = true
+vim.cmd([[let &stc='%#NonText#%{&nu?v:lnum:""}%=%{&rnu&&(v:lnum)?"\ ".v:relnum:""}%#LineNr#%{&rnu&&!(v:lnum)?"\ ".v:relnum:""} ']])
+
 require("m34t.lazy")
 -- Remap of <leader> needs to be done before loading Lazy according to Lazy docs
 require("m34t.remap")
