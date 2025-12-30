34667d4a6726952261eddbc3bed94465c432b38a
Slava Rykov <v.rykov@lamoda.ru>
2025-12-31 02:13:40 +0500

    Improve relative line number

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,9 +1,25 @@
+-- https://www.reddit.com/r/neovim/comments/1djjc6q/statuscolumn_a_beginers_guide/
+
 local gitsigns = require("gitsigns")
+
+_G.StatusColumnWithGitSigns = function()
+  if vim.wo.number and vim.g.statusline_winid == vim.api.nvim_get_current_win() then
+    return vim.v.lnum .. " " .. "%=" .. vim.v.relnum .. " " .. "%=" .. gitsigns.statuscolumn() .. "│ "
+  else
+    return ""
+  end
+end
+
+vim.opt.relativenumber = true
+vim.opt.statuscolumn = "%!v:lua.StatusColumnWithGitSigns()"
 
 gitsigns.setup({
   signs = {
     add = { text = "a┃" },
     change = { text = "c┃" },
+    delete = { text = "d_" },
+    topdelete = { text = "t‾" },
+    changedelete = { text = "~~" },
   },
   current_line_blame = true,
 })
--- lua/m34t/init.lua
+++ lua/m34t/init.lua
@@ -1,12 +1,10 @@
 vim.cmd("language en_US")
 vim.cmd([[set number]])
 vim.cmd([[set autoread]])
-vim.cmd([[set spell]])
-vim.cmd([[set spelllang=en,ru]])
-vim.cmd([[set spelloptions=camel]])
 
-vim.opt.relativenumber = true
-vim.cmd([[let &stc='%#NonText#%{&nu?v:lnum:""}%=%{&rnu&&(v:lnum)?"\ ".v:relnum:""}%#LineNr#%{&rnu&&!(v:lnum)?"\ ".v:relnum:""} ']])
+vim.opt.spell = true
+vim.opt.spelllang = "en_us,ru"
+vim.opt.spelloptions = "camel"
 
 require("m34t.lazy")
 -- Remap of <leader> needs to be done before loading Lazy according to Lazy docs
