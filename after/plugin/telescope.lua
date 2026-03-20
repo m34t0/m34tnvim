af6bdffac1e879f630c7d61a4fb60d74636fb2e9
Slava Rykov <v.rykov@lamoda.ru>
2026-03-20 13:22:24 +0500

    Add telescope resume keymap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -16,9 +16,9 @@ vim.keymap.set("n", "<leader>pg", function()
   local search_string = vim.fn.input("Grep > ")
 
   if (search_string ~= "" and search_string ~= nil) then
-    print("wtf")
     builtin.grep_string({ search = search_string })
   end
 end, { desc = "Telescope live grep but with vim grep" })
 
 vim.keymap.set("n", "<leader>ps", builtin.grep_string, { desc = "Telescope grep" })
+vim.keymap.set("n", "<leader>pt", builtin.resume, { desc = "Telescope resume last search" })
